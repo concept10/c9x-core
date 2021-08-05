35b99fdda2bcafa875df7fd412ecc8bab4b6df68
concept10 <tenthconcept@gmail.com>
2021-08-05 17:12:18 +0000

    Update

--- c9x-server-runtime-deamon/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const Transport = require('fancy-transport-agreeable-library');
-
-
-module.exports some as Transport;
-
-
---
+++ events.js
@@ -0,0 +1,7 @@
+'use strict";
+
+var Events = require('EventsEmitter');
+console.log(Events);
+module.exports = new EventsEmitter();
+
+
