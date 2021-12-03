2a091419199d36b530953c408f5b1c945d1d4adb
Tracey <tenthconcept@gmail.com>
2021-12-02 21:13:29 -0600

    Update transport.js

--- transport.js
+++ transport.js
@@ -1,19 +1,2 @@
-
-/* runtime - Manage communication with frontend via socket.io
-*/
-
-
-var Promise = require('bluebird');
-var Events = require('./events');
-
-var events = Events.create();
-var devices;
-var project = require('./project');
-
-var alarms;
-var plugins;
-var utils = require('./utils');
-
-
-var apiDevice;
-var settings;
+// transport.js
+// define configurable/modular transport
